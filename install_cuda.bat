955a565c97e81deae6f5b587d9a8071db720b81a
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-04-17 22:37:10 -0400

    Update install_cuda.bat

--- install_cuda.bat
+++ install_cuda.bat
@@ -1,15 +1,4 @@
-@echo off
+curl -L -o cuda_installer.exe https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe
+start /wait cuda_installer.exe --silent --toolkit --override --no-window --installpath=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7
+del cuda_installer.exe
 
-set cuda_url=https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_531.14_windows.exe
-set cuda_installer=cuda_installer.exe
-
-rem Download CUDA installer
-curl -L -o %cuda_installer% %cuda_url%
-
-rem Run CUDA installer in silent mode
-start /wait %cuda_installer% --silent --toolkit --override --no-window --installpath=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1
-
-rem Clean up installer file
-del %cuda_installer%
-
-echo CUDA installation complete.
