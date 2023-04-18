5c211a9653ba6380376a0aaf6c89bedcc70bb283
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-04-17 22:44:51 -0400

    Update install_cuda.bat

--- install_cuda.bat
+++ install_cuda.bat
@@ -1,4 +1,4 @@
 curl -L -o cuda_installer.exe https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe
-start /wait cuda_installer.exe --silent --toolkit --override --no-window --installpath=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7
+start /wait cuda_installer.exe -s
 del cuda_installer.exe
 
