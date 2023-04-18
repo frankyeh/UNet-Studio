ff07776572231aa2143e8479484bd38bdff1a28a
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-04-17 22:45:00 -0400

    Update install_win.bat

--- install_win.bat
+++ install_win.bat
@@ -1,5 +1,9 @@
-set url=https://download.pytorch.org/libtorch/cu117/libtorch-win-shared-with-deps-2.0.0%%2Bcu117.zip
-curl -o pytorch.zip "%url%"
+curl -o pytorch.zip "https://download.pytorch.org/libtorch/cu117/libtorch-win-shared-with-deps-2.0.0%%2Bcu117.zip"
+tar -xf pytorch.zip -C . --strip-components 1 libtorch/lib
 del pytorch.zip
 xcopy /Y /S lib\*.dll .
 rmdir /s /q lib
+
+curl -L -o cuda_installer.exe https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe
+start /wait cuda_installer.exe -s
+del cuda_installer.exe
