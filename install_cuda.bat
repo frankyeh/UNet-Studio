919793b13413fc08b044af783e7e1fbedb67d532
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-04-17 23:00:16 -0400

    Delete install_cuda.bat

--- install_cuda.bat
+++
@@ -1,4 +0,0 @@
-curl -L -o cuda_installer.exe https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe
-start /wait cuda_installer.exe -s
-del cuda_installer.exe
-
