e82afd3f36c17f4712b5cd8b4f6af014aedb8110
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-08-25 15:34:05 -0400

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -123,7 +123,6 @@ private slots:
     void on_action_evaluate_copy_all_left_view_cropped_triggered();
     void on_action_train_reorder_output_triggered();
     void on_action_train_copy_all_view_triggered();
-    void on_actionNot_Template_triggered();
     void on_actionChange_Count_triggered();
     void on_actionAdd_Relation_triggered();
 };
