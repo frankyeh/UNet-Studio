08eca103894eb6809d228f0413cade0302bff044
Yeh <frank.yeh@gmail.com>
2026-04-14 22:29:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -198,59 +198,6 @@ bool load_from_file(UNet3d& model,const char* file_name)
             std::copy(data,data+buffer.numel(),buffer.data_ptr<int64_t>());
         ++id;
     }
-    /*
-    tipl::shape<3> image_volume(6,6,6);
-    std::vector<float> data(image_volume.size());
-    for(size_t i = 0;i < data.size();++i)
-        data[i] = i;
-
-    model->train(false);
-    model->print_layers();
-
-    auto cn = model->encoding[0]->begin();
-    auto re = cn+1;
-    auto bn = re+1;
-    auto cn2 = bn+1;
-    auto max_pool3d = model->encoding[1]->begin();
-    auto upsampling = model->up[0]->begin();
-    auto data_tensor = torch::from_blob(&data[0],{1,1,int(image_volume[2]),int(image_volume[1]),int(image_volume[0])});
-    //auto out_tensor = upsampling->forward(max_pool3d->forward(bn->forward(re->forward(cn->forward(data_tensor)))));
-    auto out_tensor = cn2->forward(bn->forward(re->forward(cn->forward(data_tensor))));
-    tipl::out() << out_tensor.sizes();
-
-
-    tipl::ml3d::network n;
-    //n << new conv_3d(1,8) << (new relu(8)) << new batch_norm_3d(8) << new max_pool_3d(8) << new upsample_3d(8);
-    n << new tipl::ml3d::conv_3d(1,8)
-      << new tipl::ml3d::relu(8)
-      << new tipl::ml3d::batch_norm_3d(8)
-      << new tipl::ml3d::conv_3d(8,8);
-    n.print(std::cout);
-
-    auto dim = image_volume;
-    n.init_image(dim);
-    auto params = n.parameters();
-
-
-    auto ptr1 = model->parameters()[0].data_ptr<float>();
-    auto ptr2 = model->parameters()[1].data_ptr<float>();
-    auto ptr3 = model->parameters()[2].data_ptr<float>();
-    auto ptr4 = model->parameters()[3].data_ptr<float>();
-    auto ptr5 = model->parameters()[4].data_ptr<float>();
-    auto ptr6 = model->parameters()[5].data_ptr<float>();
-
-    std::copy(ptr1,ptr1+8*3*3*3,params[0].first);
-    std::copy(ptr2,ptr2+8,params[1].first);
-    std::copy(ptr3,ptr3+8,params[2].first);
-    std::copy(ptr4,ptr4+8,params[3].first);
-    std::copy(ptr5,ptr5+8*8*3*3*3,params[4].first);
-    std::copy(ptr6,ptr6+8,params[5].first);
-
-    auto ptr = n.forward(&data[0]);
-
-    for(size_t i = 0;i < out_tensor.numel();++i)
-        tipl::out() << out_tensor.data_ptr<float>()[i] << "\t" << ptr[i] << "\t" << ptr[i] - out_tensor.data_ptr<float>()[i];
-    */
     return true;
 }
 bool save_to_file(UNet3d& model,const char* file_name)
@@ -295,9 +242,8 @@ std::string show_structure(const UNet3d& model)
 {
     std::ostringstream out;
     std::vector<int> ks;
-    auto features = model->parse_feature_string(ks);
-    std::vector<std::vector<int> > features_down(std::move(features.first));
-    std::vector<std::vector<int> > features_up(std::move(features.second));
+    std::vector<std::vector<int> > features_down,features_up;
+    tipl::ml3d::parse_feature_string(model->feature_string,model->in_count,features_down,features_up,ks);
 
     for(int level=0; level< features_down.size(); level++)
     {
